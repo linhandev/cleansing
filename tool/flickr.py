c1b4464f355f2a49926e78914c4000934221e850
Lin Han <me@linhan.email>
2022-05-18 07:05:32 -0400

    32

--- tool/flickr.py
+++ tool/flickr.py
@@ -43,6 +43,7 @@ for keyword in [
     total += kw_total
     kw_total = 0
     kw_total += i
+
     print(keyword, ", keyword total", kw_total, ", dataset total", total)
     i = 0
 
@@ -51,74 +52,77 @@ for keyword in [
         os.makedirs(save_dir)
 
     for page_idx in range(10):
-        print("page_idx", page_idx)
-        page = flickr.photos.search(
-            text=keyword,
-            tag_mode="all",
-            tags=keyword + ",HD",
-            extras="url_c",
-            sort="relevance",
-            per_page=500,
-            page=page_idx,
-            content_type=1,
-        )
+        try:
+            print("page_idx", page_idx)
+            page = flickr.photos.search(
+                text=keyword,
+                tag_mode="all",
+                tags=keyword + ",HD",
+                extras="url_c",
+                sort="relevance",
+                per_page=500,
+                page=page_idx,
+                content_type=1,
+            )
+        except:
+            time.sleep(10)
 
         import warnings
 
         for photo in tqdm(page.findall("./photos/photo")):
             tic = time.time()
+            try:
+                size_res = flickr.photos.getSizes(photo_id=photo.get("id"))
+                sizes = []
+                for size in size_res.findall("./sizes/size"):
+                    try:
+                        sizes.append((int(size.get("height")), size.get("source")))
+                    except:
+                        pass
+                sizes.sort(key=lambda s: s[0], reverse=True)
+                if sizes[0][0] < 1024:
+                    continue
+                # print([s[0] for s in sizes])
+                fav_res = flickr.photos.getFavorites(photo_id=photo.get("id"))
+                # print(ET.tostring(fav_res))
+                # print(fav_res.find("photo").get('total'))
+                if fav_res.find("photo").get("total") == 0:
+                    continue
 
-            size_res = flickr.photos.getSizes(photo_id=photo.get("id"))
-            sizes = []
-            for size in size_res.findall("./sizes/size"):
-                try:
-                    sizes.append((int(size.get("height")), size.get("source")))
-                except:
-                    pass
-            sizes.sort(key=lambda s: s[0], reverse=True)
-            if sizes[0][0] < 1024:
-                continue
-            # print([s[0] for s in sizes])
-            fav_res = flickr.photos.getFavorites(photo_id=photo.get("id"))
-            # print(ET.tostring(fav_res))
-            # print(fav_res.find("photo").get('total'))
-            if fav_res.find("photo").get("total") == 0:
-                continue
+                for size in sizes:
+                    if size[0] < 1024:
+                        url = size[1]
+                        break
 
-            for size in sizes:
-                if size[0] < 1024:
-                    url = size[1]
-                    break
+                if url is not None:
+                        response = requests.get(url)
+                        file = BytesIO(response.content)
 
-            if url is not None:
-                try:
-                    response = requests.get(url)
-                    file = BytesIO(response.content)
+                        # Read image from file
 
-                    # Read image from file
+                        img = skimage.io.imread(file)
 
-                    img = skimage.io.imread(file)
+                        # Resize images
+                        s = img.shape
+                        if len(s) != 3:
+                            continue
 
-                    # Resize images
-                    s = img.shape
-                    if len(s) != 3:
-                        continue
+                        img = skimage.transform.resize(
+                            img, (512, 512), order=2, mode="constant", anti_aliasing=True
+                        )
 
-                    img = skimage.transform.resize(
-                        img, (512, 512), order=2, mode="constant", anti_aliasing=True
-                    )
+                        # Convert to uint8, suppress the warning about the precision loss
+                        with warnings.catch_warnings():
+                            warnings.simplefilter("ignore")
+                            img = skimage.img_as_ubyte(img)
 
-                    # Convert to uint8, suppress the warning about the precision loss
-                    with warnings.catch_warnings():
-                        warnings.simplefilter("ignore")
-                        img = skimage.img_as_ubyte(img)
+                        save_path = osp.join(save_dir, f"{keyword}_{str(i).zfill(5)}.png")
 
-                    save_path = osp.join(save_dir, f"{keyword}_{str(i).zfill(5)}.png")
+                        skimage.io.imsave(save_path, img)
 
-                    skimage.io.imsave(save_path, img)
-
-                    i = i + 1
-                except Exception as e:
-                    print("error", e)
+                        i = i + 1
+            except Exception as e:
+                print("error", e)
+                time.sleep(5)
             
             time.sleep(max(2 - (time.time() - tic), 0)) # download at most 3k6/h
